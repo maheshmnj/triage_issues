cfff751280fba007780204b9d2064b828f9eb497
mahesh jamdade <maheshmn121@gmail.com>
2021-09-22 19:37:32 +0530

    update release mode

--- apps/video_issue/build/web/flutter_service_worker.js
+++ apps/video_issue/build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "18dab819a8f5d1698c5520436a78a866",
-"index.html": "b0ff09b37bbb78388ea12608a077528b",
-"/": "b0ff09b37bbb78388ea12608a077528b",
-"main.dart.js": "80bc20aeef0cdba012afb1d2a33183bf",
+"index.html": "6aca7eba2e528f501f35c17f3b68e044",
+"/": "6aca7eba2e528f501f35c17f3b68e044",
+"main.dart.js": "801391e7004ac241a5537f2738122f2a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
