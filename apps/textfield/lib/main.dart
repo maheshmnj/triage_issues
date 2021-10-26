bfa004ad0e6a9b7ab1fc9e4f746ee5ba3b9f84da
mahesh jamdade <maheshmn121@gmail.com>
2021-10-26 13:01:11 +0530

    add more examples

--- apps/textfield/lib/main.dart
+++ apps/textfield/lib/main.dart
@@ -14,14 +14,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Text Input Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -29,181 +28,140 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  final String version = 'stable v2.5.3';
   final TextEditingController _searchCtrl = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    double shortestSide = MediaQuery.of(context).size.shortestSide;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        actions: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('$version'),
-            ],
-          )
-        ],
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: 'textfield'),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: 'textFormField'),
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              IconButton(
-                  icon: const Icon(
-                    Icons.search,
-                    color: Colors.red,
-                    size: 40,
-                  ),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return Dialog(
-                          child: Container(
-                            width: 250,
-                            height: 150,
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 10),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  width: 210,
-                                  child: TextField(
-                                    controller: _searchCtrl,
-                                    cursorColor: Colors.red,
-                                    autofocus: true,
-                                    textInputAction: TextInputAction.done,
-                                    decoration: InputDecoration(
-                                      labelText: "Search by Place Name",
-                                      labelStyle:
-                                          const TextStyle(color: Colors.red),
-                                      filled: true,
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(4),
-                                        borderSide: const BorderSide(
-                                          width: 1,
-                                          color: Colors.grey,
-                                          style: BorderStyle.solid,
-                                        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            IconButton(
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.red,
+                  size: 40,
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Dialog(
+                        child: Container(
+                          width: shortestSide > 600 ? 400 : 250,
+                          height: 150,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: shortestSide > 600 ? 360 : 210,
+                                child: TextField(
+                                  controller: _searchCtrl,
+                                  cursorColor: Colors.red,
+                                  autofocus: true,
+                                  textInputAction: TextInputAction.done,
+                                  decoration: InputDecoration(
+                                    labelText: "Search by Place Name",
+                                    labelStyle:
+                                        const TextStyle(color: Colors.red),
+                                    filled: true,
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(4),
+                                      borderSide: const BorderSide(
+                                        width: 1,
+                                        color: Colors.grey,
+                                        style: BorderStyle.solid,
                                       ),
-                                      focusedBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(4),
-                                        borderSide: const BorderSide(
-                                          width: 2,
-                                          color: Colors.red,
-                                          style: BorderStyle.solid,
-                                        ),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(4),
+                                      borderSide: const BorderSide(
+                                        width: 2,
+                                        color: Colors.red,
+                                        style: BorderStyle.solid,
                                       ),
                                     ),
                                   ),
                                 ),
-                                const SizedBox(
-                                  height: 15,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    ElevatedButton.icon(
-                                      style: ElevatedButton.styleFrom(
-                                        primary: Colors.green,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          side: const BorderSide(
-                                            color: Colors.green,
-                                            width: 1,
-                                          ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  ElevatedButton.icon(
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.green,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        side: const BorderSide(
+                                          color: Colors.green,
+                                          width: 1,
                                         ),
                                       ),
-                                      label: const Text(
-                                        "Search",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                        ),
-                                      ),
-                                      icon: const Icon(
-                                        Icons.search_rounded,
-                                      ),
-                                      onPressed: () {
-                                        FocusScope.of(context).unfocus();
-                                        Navigator.pop(context);
-                                      },
                                     ),
-                                    const SizedBox(
-                                      width: 20,
+                                    label: Text(
+                                      "Search",
+                                      style: TextStyle(
+                                        fontSize: width < 600 ? 12 : 18,
+                                      ),
                                     ),
-                                    ElevatedButton.icon(
-                                      style: ElevatedButton.styleFrom(
-                                        primary: Colors.red,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          side: const BorderSide(
-                                            color: Colors.red,
-                                            width: 1,
-                                          ),
+                                    icon: const Icon(
+                                      Icons.search_rounded,
+                                    ),
+                                    onPressed: () {
+                                      FocusScope.of(context).unfocus();
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                  SizedBox(
+                                    width: width < 600 ? 5 : 20,
+                                  ),
+                                  ElevatedButton.icon(
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.red,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        side: const BorderSide(
+                                          color: Colors.red,
+                                          width: 1,
                                         ),
                                       ),
-                                      label: const Text(
-                                        "Clear",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                        ),
+                                    ),
+                                    label: Text(
+                                      "Clear",
+                                      style: TextStyle(
+                                        fontSize: width < 600 ? 12 : 18,
                                       ),
-                                      icon: const Icon(Icons.clear),
-                                      onPressed: () {
-                                        _searchCtrl.clear();
-                                        FocusScope.of(context).unfocus();
-                                        Navigator.pop(context);
-                                      },
-                                    )
-                                  ],
-                                )
-                              ],
-                            ),
+                                    ),
+                                    icon: const Icon(Icons.clear),
+                                    onPressed: () {
+                                      _searchCtrl.clear();
+                                      FocusScope.of(context).unfocus();
+                                      Navigator.pop(context);
+                                    },
+                                  )
+                                ],
+                              )
+                            ],
                           ),
-                        );
-                      },
-                    );
-                  }),
-            ],
-          ),
+                        ),
+                      );
+                    },
+                  );
+                }),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/waveform.dart
@@ -0,0 +1,123 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: FutureBuilder(
+        future: loadGraph(),
+        builder: (context, AsyncSnapshot<Int16List> waveData) {
+          return (!waveData.hasData)
+              ? const CircularProgressIndicator.adaptive()
+              : SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 20,
+                      height: MediaQuery.of(context).size.height,
+                      child: RepaintBoundary(
+                        child: CustomPaint(
+                          willChange: false,
+                          isComplex: true,
+                          painter: PaintTest(
+                            waveData: waveData.data!,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+        },
+      ),
+    );
+  }
+}
+
+class PaintTest extends CustomPainter {
+  final Int16List waveData;
+
+  const PaintTest({
+    required this.waveData,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final _height = size.height;
+    double x = 0;
+    double strokeSize = .5;
+    const zoomFactor = .5;
+
+    final Paint paintPos = Paint()
+      ..color = Colors.pink
+      ..strokeWidth = strokeSize
+      ..isAntiAlias = false
+      ..style = PaintingStyle.stroke;
+
+    final Paint paintNeg = Paint()
+      ..color = Colors.pink
+      ..strokeWidth = strokeSize
+      ..isAntiAlias = false
+      ..style = PaintingStyle.stroke;
+
+    final Paint paintZero = Paint()
+      ..color = Colors.green
+      ..strokeWidth = strokeSize
+      ..isAntiAlias = false
+      ..style = PaintingStyle.stroke;
+
+    int index = 0;
+    for (index = 0; index < waveData.length; index++) {
+      if (waveData[index].isNegative) {
+        canvas.drawLine(
+            Offset(x, _height * 1 / 2),
+            Offset(
+                x, _height * 1 / 2 - waveData[index] / 32768 * (_height / 2)),
+            paintPos);
+        x += zoomFactor;
+      } else {
+        (waveData[index] == 0)
+            ? canvas.drawLine(Offset(x, _height * 1 / 2),
+                Offset(x, _height * 1 / 2 + 1), paintZero)
+            : canvas.drawLine(
+                Offset(x, _height * 1 / 2),
+                Offset(x,
+                    _height * 1 / 2 - waveData[index] / 32767 * (_height / 2)),
+                paintNeg);
+        x += zoomFactor;
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+Future<Int16List> loadGraph() async {
+  //*_____________________________________________
+  //! LOAD ANY WAVE FILE - THIS IS 16bit 44.1 MONO
+  //*_____________________________________________
+  Int16List load16bitMonoWave = await rootBundle
+      .load('assets/twinkle_twinkle_little_star.wav')
+      .then((value) => value.buffer.asInt16List());
+  //! THIN WAVE FILE TO 1/20
+  Int16List waveDataThinned =
+      Int16List((load16bitMonoWave.length * 1 / 20).round());
+  int reducedIndex = 0;
+  for (int index = 0; index + 20 < load16bitMonoWave.length; index += 20) {
+    waveDataThinned[reducedIndex] = load16bitMonoWave[index];
+    reducedIndex++;
+  }
+  return waveDataThinned;
+}
