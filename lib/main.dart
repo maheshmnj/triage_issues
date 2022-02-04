329e05ffb94596a9c24a81eaebcd31eaffcbd2b5
mahesh jamdade <maheshmn121@gmail.com>
2022-02-04 17:58:15 +0530

    add google maps sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,59 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:routes_issue/search.dart';
-import 'package:routes_issue/video.dart';
-import 'package:routes_issue/waveform.dart';
-// import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import 'background_notifier.dart';
-import 'camera.dart';
-import 'image_picker.dart';
+void main() => runApp(MyApp());
 
-void main() async {
-  // setUrlStrategy(PathUrlStrategy());
-  runApp(MaterialApp(
-    initialRoute: "/home",
-    routes: <String, WidgetBuilder>{
-      '/home': (BuildContext context) => Home(),
-      '/BackgroundNotifier': (BuildContext context) => const BackgroundNotifier(
-            title: 'BackgroundNotifier',
-          ),
-      '/wave_form': (BuildContext context) => const WaveFormSample(),
-      '/image_picker': (BuildContext context) => ImagePickerSample(
-            title: 'image picker sample',
-          ),
-      '/video_app': (BuildContext context) => VideoApp(),
-      '/camera_app': (BuildContext context) => CameraExampleHome(),
-      '/search_app': (BuildContext context) => SearchDemo(),
-    },
-    onUnknownRoute: (RouteSettings settings) {
-      return MaterialPageRoute<void>(
-        settings: settings,
-        builder: (BuildContext context) =>
-            const Scaffold(body: Center(child: Text('404 Not Found'))),
-      );
-    },
-  ));
-}
-
-class Home extends StatelessWidget {
-  List<String> widgets = [
-    'image_picker',
-    'wave_form',
-    'BackgroundNotifier',
-    'video_app',
-    'camera_app',
-    'search_app'
-  ];
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Screen 1')),
-        body: ListView.separated(
-            separatorBuilder: (context, index) => const Divider(),
-            itemCount: widgets.length,
-            itemBuilder: (x, y) {
-              return ListTile(
-                title: Text('${widgets[y]} sample'),
-                onTap: () {
-                  Navigator.pushNamed(context, '/${widgets[y]}');
-                },
-              );
-            }));
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
   }
 }
 
-class Screen2 extends StatelessWidget {
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Screen 2'),
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Example to show case loading local asset',
-            ),
-            Image.asset(
-              'assets/snippet.png',
-            ),
-            Text(
-              '100',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
       ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
