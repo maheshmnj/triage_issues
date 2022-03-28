5e44bd52f0c6ac95a4ca705bb1c3d90f644da0b2
mahesh jamdade <maheshmn121@gmail.com>
2022-03-28 17:13:12 +0530

    add sample app for overflow text in container

---
+++ apps/sample/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: PageOne(),
+    );
+  }
+}
+
+class PageOne extends StatelessWidget {
+  const PageOne({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Container(
+        width: 400,
+        color: Colors.green,
+        child: const Text(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          style: TextStyle(fontSize: 18, color: Colors.black),
+        ),
+      ),
+    ));
+  }
+}
