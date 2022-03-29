b7f7dace5946110d8e529321a2fcb19368ee4e29
mahesh jamdade <maheshmn121@gmail.com>
2022-03-29 11:16:09 +0530

    add chinese sample

--- apps/sample/lib/main.dart
+++ apps/sample/lib/main.dart
@@ -1,35 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: PageOne(),
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
+      home: Home(),
     );
   }
 }
 
-class PageOne extends StatelessWidget {
-  const PageOne({Key? key}) : super(key: key);
+class Home extends StatelessWidget {
+  Home({Key? key}) : super(key: key);
+
+  final List widget = ["运动打卡", "抽奖"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Container(
-        width: 400,
-        color: Colors.green,
-        child: const Text(
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          style: TextStyle(fontSize: 18, color: Colors.black),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text(
+          '功能',
+          style: TextStyle(
+            color: Color(0xFFFFFFFF),
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ),
-    ));
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (int index = 0; index < widget.length; index++)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      widget[index],
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
   }
 }
